27c76fc08313451359a1fa9eea91e2ce9d311189
Enrique Rodriguez <enrique.rodriguez9421@alumnos.udg.mx>
2020-10-04 15:36:50 -0500

    Header de la estrucura de los personajes

---
+++ personaje.h
@@ -0,0 +1,49 @@
+#ifndef PERSONAJE_H
+#define PERSONAJE_H
+
+#include<stdio.h>
+
+struct personaje
+{
+    char nombre[20];
+    char tipo[20];
+    float fuerza;
+    int salud;
+}personajes[5];
+
+unsigned int cont = 0;
+
+void capturar_personajes()
+{
+    if(cont<5)
+    {
+        printf("Nombre: ");
+        scanf("%s", personajes[cont].nombre);
+        printf("Tipo: ");
+        scanf("%s", personajes[cont].tipo);
+        printf("Fuerza: ");
+        scanf("%f", &personajes[cont].fuerza);
+        printf("Salud: ");
+        scanf("%i", &personajes[cont].salud);
+        cont++;
+    }
+    else
+    {
+        printf("El arreglo de personajes está lleno\n");
+    }
+    
+}
+
+void mostrar_personaje()
+{
+    for(size_t i=0; i<cont; i++)
+    {
+        printf("Nombre: %s\n", personajes[i].nombre);
+        printf("Tipo: %s\n", personajes[i].tipo);
+        printf("Fuerza: %f\n", personajes[i].fuerza);
+        printf("Salud: %i\n\n", personajes[i].salud);
+    }
+}
+
+
+#endif
